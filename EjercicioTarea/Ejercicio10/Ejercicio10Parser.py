d9d173fb6415102a3ab8000ca693522d8ceab37b
Eduardo <e.aguilera.a98@gamail.com>
2026-07-03 10:27:28 -0600

    Se agrega ejercicio 10: Print (Cadena)

---
+++ EjercicioTarea/Ejercicio10/Ejercicio10Parser.py
@@ -0,0 +1,130 @@
+# Generated from ./Ejercicio10.g4 by ANTLR 4.13.2
+# encoding: utf-8
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+	from typing import TextIO
+else:
+	from typing.io import TextIO
+
+def serializedATN():
+    return [
+        4,1,6,10,2,0,7,0,2,1,7,1,1,0,1,0,1,0,1,1,1,1,1,1,0,0,2,0,2,0,0,7,
+        0,4,1,0,0,0,2,7,1,0,0,0,4,5,3,2,1,0,5,6,5,0,0,1,6,1,1,0,0,0,7,8,
+        5,0,0,1,8,3,1,0,0,0,0
+    ]
+
+class Ejercicio10Parser ( Parser ):
+
+    grammarFileName = "Ejercicio10.g4"
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    sharedContextCache = PredictionContextCache()
+
+    literalNames = [ "<INVALID>", "'print'", "<INVALID>", "'('", "')'", 
+                     "';'" ]
+
+    symbolicNames = [ "<INVALID>", "PRINT", "CADENA", "PARENTHESIS_OPEN", 
+                      "PARENTHESIS_CLOSE", "SC", "WS" ]
+
+    RULE_root = 0
+    RULE_expr = 1
+
+    ruleNames =  [ "root", "expr" ]
+
+    EOF = Token.EOF
+    PRINT=1
+    CADENA=2
+    PARENTHESIS_OPEN=3
+    PARENTHESIS_CLOSE=4
+    SC=5
+    WS=6
+
+    def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.13.2")
+        self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
+        self._predicates = None
+
+
+
+
+    class RootContext(ParserRuleContext):
+        __slots__ = 'parser'
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+        def expr(self):
+            return self.getTypedRuleContext(Ejercicio10Parser.ExprContext,0)
+
+
+        def EOF(self):
+            return self.getToken(Ejercicio10Parser.EOF, 0)
+
+        def getRuleIndex(self):
+            return Ejercicio10Parser.RULE_root
+
+
+
+
+    def root(self):
+
+        localctx = Ejercicio10Parser.RootContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 0, self.RULE_root)
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 4
+            self.expr()
+            self.state = 5
+            self.match(Ejercicio10Parser.EOF)
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+    class ExprContext(ParserRuleContext):
+        __slots__ = 'parser'
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+        def EOF(self):
+            return self.getToken(Ejercicio10Parser.EOF, 0)
+
+        def getRuleIndex(self):
+            return Ejercicio10Parser.RULE_expr
+
+
+
+
+    def expr(self):
+
+        localctx = Ejercicio10Parser.ExprContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 2, self.RULE_expr)
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 7
+            self.match(Ejercicio10Parser.EOF)
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+
+
+
