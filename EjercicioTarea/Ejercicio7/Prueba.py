ff75549f70d1c8399c13a34f02c87cd6104d8109
Eduardo <e.aguilera.a98@gamail.com>
2026-07-03 10:24:08 -0600

    Se agrega ejercicio 7: Análisis Léxico, int total = 100

---
+++ EjercicioTarea/Ejercicio7/Prueba.py
@@ -0,0 +1,24 @@
+from antlr4 import *
+
+from Ejercicio7Lexer import Ejercicio7Lexer
+import sys
+
+input_stream = FileStream(sys.argv[1])
+
+lexer = Ejercicio7Lexer(input_stream)
+
+tokens = CommonTokenStream(lexer)
+tokens.fill()
+
+print(tokens)
+
+for token in tokens.tokens:
+    print("Texto ", token.text)
+    print("Tipo ", token.type)
+    print("Linea ", token.line)
+    print("columna ", token.column)
+    nombre_token = lexer.symbolicNames[token.type]
+
+    print("Nombre del token: ", nombre_token)
+
+    print("---------------------")
